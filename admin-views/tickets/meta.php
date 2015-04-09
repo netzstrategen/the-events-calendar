eac21df0066d9cfa3c3128d1a1405ef366fb5ea3
Kyle Unzicker <kyle.unzicker@gmail.com>
2015-04-09 15:19:38 -0500

    admin ui markup updates

--- admin-views/tickets/meta.php
+++ admin-views/tickets/meta.php
@@ -1,89 +1,205 @@
-<table class="eventtable ticket_list ticket_meta_form">
-	<tbody>
-	<tr>
-		<td colspan="2" class="titlewrap">
-			<h4 class="tribe_sectionheader">Attendee Information</h4>
-		</td>
-	</tr>
-	<tr>
-		<td style="width:170px"><?php _e( 'Use Saved Attendee Info Fields:', 'tribe-events-calendar' ); ?></td>
-		<td>
-			<select class="chosen ticket-attendee-info-dropdown" name="ticket-attendee-info[MetaID]"
-			        id="saved_ticket-user-meta" title="ticket-attendee-info[MetaID]">
-				<option value="0" selected="selected">Use New Attendee Info Fields</option>
-			</select>
-		</td>
-	</tr>
-	<tr>
-		<td style="width:170px"><?php _e( 'Attnedee Info Fields Group Name:', 'tribe-events-calendar' ); ?></td>
-		<td>
-			<input type="text" name="ticket-attendee-info[MetaFieldsName]" size="25" value="">
-		</td>
-	</tr>
-	<tr>
-		<td style="width:170px">
-			<h5><?php _e( 'Add New Field:', 'tribe-events-calendar' ); ?></h5>
-			<ul class="tribe-tickets-attendee-info-options">
-				<li class="tribe-tickets-attendee-info-option">
-					<a href="">Text <span class="dashicons dashicons-plus-alt"></span></a>
-				</li>
-				<li class="tribe-tickets-attendee-info-option">
-					<a href="">Radio <span class="dashicons dashicons-plus-alt"></span></a>
-				</li>
-				<li class="tribe-tickets-attendee-info-option">
-					<a href="">Checkbox <span class="dashicons dashicons-plus-alt"></span></a>
-				</li>
-				<li class="tribe-tickets-attendee-info-option">
-					<a href="">Select <span class="dashicons dashicons-plus-alt"></span></a>
-				</li>
-			</ul>
+<tr class="eventtable ticket_list tribe_tickets_attendee_info_form">
+	<td colspan="2">
+		<table class="eventtable">
+			<tr>
+				<td colspan="2" class="titlewrap">
+					<h4 class="tribe_sectionheader">Attendee Information</h4>
+				</td>
+			</tr>
+			<tr>
+				<td style="width:170px"><?php _e( 'Use Saved Attendee Info Fields:', 'tribe-events-calendar' ); ?></td>
+				<td>
+					<select class="chosen ticket-attendee-info-dropdown" name="ticket-attendee-info[MetaID]"
+					        id="saved_ticket-user-meta" title="ticket-attendee-info[MetaID]">
+						<option value="0" selected="selected">Use New Attendee Info Fields</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td style="width:170px"><?php _e( 'Attnedee Info Fields Group Name:', 'tribe-events-calendar' ); ?></td>
+				<td>
+					<input type="text" name="ticket-attendee-info[MetaFieldsName]" size="25" value="">
+				</td>
+			</tr>
+			<tr>
+				<td style="width:170px">
+					<h5><?php _e( 'Add New Field:', 'tribe-events-calendar' ); ?></h5>
+					<ul class="tribe-tickets-attendee-info-options">
+						<li id="tribe-tickets-add-text" class="tribe-tickets-attendee-info-option">
+							<a href="">Text <span class="dashicons dashicons-plus-alt"></span></a>
+						</li>
+						<li id="tribe-tickets-add-number" class="tribe-tickets-attendee-info-option">
+							<a href="">Number <span class="dashicons dashicons-plus-alt"></span></a>
+						</li>
+						<li id="tribe-tickets-add-radio" class="tribe-tickets-attendee-info-option">
+							<a href="">Radio <span class="dashicons dashicons-plus-alt"></span></a>
+						</li>
+						<li id="tribe-tickets-add-checkbox" class="tribe-tickets-attendee-info-option">
+							<a href="">Checkbox <span class="dashicons dashicons-plus-alt"></span></a>
+						</li>
+						<li id="tribe-tickets-add-select" class="tribe-tickets-attendee-info-option">
+							<a href="">Select <span class="dashicons dashicons-plus-alt"></span></a>
+						</li>
+					</ul>
 
-		</td>
-		<td>
-			<h5><?php _e( 'Active Fields:', 'tribe-events-calendar' ); ?></h5>
-			<div id="tribe-tickets-attendee-sortables" class="meta-box-sortables ui-sortable">
-				<div id="tickets_attendee_info_field_1" class="tribe-tickets-attendee-info-active-field tribe-tickets-attendee-field postbox closed">
-					<div class="handlediv" title="Click to toggle"><br></div>
-					<h3 class="hndle ui-sortable-handle"><span>Text:</span> First Name</h3>
+				</td>
+				<td>
+					<h5><?php _e( 'Active Fields:', 'tribe-events-calendar' ); ?></h5>
 
-					<div class="inside">
-						<div class="tribe-tickets-input tribe-tickets-input-text">
-							<label for="tickets_attendee_info_field">Label:</label>
-							<input type="text" name="tickets_attendee_info_field" value="">
+					<div id="tribe-tickets-attendee-sortables" class="meta-box-sortables ui-sortable">
+						<?php /* adding a unique ID to each item here */ ?>
+						<div id="tickets_attendee_info_field_1"
+						     class="tribe-tickets-attendee-info-active-field postbox closed">
+							<div class="handlediv" title="Click to toggle"><br></div>
+							<?php
+							/*
+								heading text should be populated by the value of the label field
+								would be neat if it updated in real time as you typed in the label field
+							 */
+							?>
+							<h3 class="hndle ui-sortable-handle"><span>Text:</span> First Name</h3>
+
+							<div class="inside">
+								<?php /* Begin text field */ ?>
+								<div class="tribe-tickets-input tribe-tickets-input-text">
+									<label for="tickets_attendee_info_field">Label:</label>
+									<input type="text" name="tickets_attendee_info_field" value="">
+								</div>
+								<?php /* adding a class based on the input type (tribe-tickets-input-checkbox) */ ?>
+								<div class="tribe-tickets-input tribe-tickets-input-checkbox tribe-tickets-required">
+									<label class="prompt"><input type="checkbox"
+									                             name="tickets_attendee_info_field_required">
+										Required?
+									</label>
+								</div>
+								<div class="tribe-tickets-delete-field">
+									<a href="#">Delete this field</a>
+								</div>
+								<?php /* End text field */ ?>
+							</div>
 						</div>
-						<div class="tribe-tickets-input tribe-tickets-input-checkbox tribe-tickets-required">
-							<label class="prompt"><input type="checkbox" name="tickets_attendee_info_field_required">
-								Required?</label>
+						<?php /* adding a unique ID to each item here */ ?>
+						<div id="tickets_attendee_info_field_2"
+						     class="tribe-tickets-attendee-info-active-field postbox closed">
+							<div class="handlediv" title="Click to toggle"><br></div>
+							<h3 class="hndle ui-sortable-handle"><span>Radio:</span></h3>
+
+							<div class="inside">
+								<?php /* Begin radio field */ ?>
+								<div class="tribe-tickets-input tribe-tickets-input-text">
+									<label for="tickets_attendee_info_field">Label:</label>
+									<input type="text" name="tickets_attendee_info_field" value="">
+								</div>
+								<div class="tribe-tickets-input tribe-tickets-input-checkbox tribe-tickets-required">
+									<label class="prompt"><input type="checkbox"
+									                             name="tickets_attendee_info_field_required">
+										Required?
+									</label>
+								</div>
+								<div class="tribe-tickets-input tribe-tickets-input-textarea">
+									<label for="tickets_attendee_info_field">Options (one per line)</label>
+									<textarea name="tickets_attendee_info_field" value="" rows="5"></textarea>
+								</div>
+								<div class="tribe-tickets-input tribe-tickets-input-text">
+									<label for="tickets_attendee_info_field">Default Value:</label>
+									<input type="text" name="tickets_attendee_info_field" value="">
+								</div>
+								<div class="tribe-tickets-delete-field">
+									<a href="#">Delete this field</a>
+								</div>
+								<?php /* End radio field */ ?>
+							</div>
 						</div>
-						<div class="tribe-tickets-delete-field">
-							<a href="#">Delete this field</a>
+						<?php /* adding a unique ID to each item here */ ?>
+						<div id="tickets_attendee_info_field_3"
+						     class="tribe-tickets-attendee-info-active-field postbox closed">
+							<div class="handlediv" title="Click to toggle"><br></div>
+							<h3 class="hndle ui-sortable-handle"><span>Checkbox:</span></h3>
+
+							<div class="inside">
+								<?php /* Begin Checkbox field */ ?>
+								<div class="tribe-tickets-input tribe-tickets-input-text">
+									<label for="tickets_attendee_info_field">Label:</label>
+									<input type="text" name="tickets_attendee_info_field" value="">
+								</div>
+								<div class="tribe-tickets-input tribe-tickets-input-checkbox tribe-tickets-required">
+									<label class="prompt"><input type="checkbox"
+									                             name="tickets_attendee_info_field_required">
+										Required?
+									</label>
+								</div>
+								<div class="tribe-tickets-input tribe-tickets-input-textarea">
+									<label for="tickets_attendee_info_field">Options (one per line)</label>
+									<textarea name="tickets_attendee_info_field" value="" rows="5"></textarea>
+								</div>
+								<div class="tribe-tickets-delete-field">
+									<a href="#">Delete this field</a>
+								</div>
+								<?php /* End checkbox field */ ?>
+							</div>
+						</div>
+						<?php /* adding a unique ID to each item here */ ?>
+						<div id="tickets_attendee_info_field_4"
+						     class="tribe-tickets-attendee-info-active-field postbox closed">
+							<div class="handlediv" title="Click to toggle"><br></div>
+							<h3 class="hndle ui-sortable-handle"><span>Select:</span></h3>
+
+							<div class="inside">
+								<?php /* Begin select field */ ?>
+								<div class="tribe-tickets-input tribe-tickets-input-text">
+									<label for="tickets_attendee_info_field">Label:</label>
+									<input type="text" name="tickets_attendee_info_field" value="">
+								</div>
+								<div class="tribe-tickets-input tribe-tickets-input-textarea">
+									<label for="tickets_attendee_info_field">Options (one per line)</label>
+									<textarea name="tickets_attendee_info_field" value="" rows="5"></textarea>
+								</div>
+								<div class="tribe-tickets-input tribe-tickets-input-checkbox tribe-tickets-required">
+									<label class="prompt"><input type="checkbox"
+									                             name="tickets_attendee_info_field_required">
+										Required?
+									</label>
+								</div>
+								<div class="tribe-tickets-delete-field">
+									<a href="#">Delete this field</a>
+								</div>
+								<?php /* End select field */ ?>
+							</div>
+						</div>
+						<?php /* adding a unique ID to each item here */ ?>
+						<div id="tickets_attendee_info_field_5"
+						     class="tribe-tickets-attendee-info-active-field postbox closed">
+							<div class="handlediv" title="Click to toggle"><br></div>
+							<?php
+							/*
+								heading text should be populated by the value of the label field
+								would be neat if it updated in real time as you typed in the label field
+							 */
+							?>
+							<h3 class="hndle ui-sortable-handle"><span>Number:</span></h3>
+
+							<div class="inside">
+								<?php /* Begin text field */ ?>
+								<div class="tribe-tickets-input tribe-tickets-input-text">
+									<label for="tickets_attendee_info_field">Label:</label>
+									<input type="text" name="tickets_attendee_info_field" value="">
+								</div>
+								<?php /* adding a class based on the input type (tribe-tickets-input-checkbox) */ ?>
+								<div class="tribe-tickets-input tribe-tickets-input-checkbox tribe-tickets-required">
+									<label class="prompt"><input type="checkbox"
+									                             name="tickets_attendee_info_field_required">
+										Required?
+									</label>
+								</div>
+								<div class="tribe-tickets-delete-field">
+									<a href="#">Delete this field</a>
+								</div>
+								<?php /* End text field */ ?>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div id="tickets_attendee_info_field_2" class="tribe-tickets-attendee-info-active-field tribe-tickets-attendee-field postbox closed">
-					<div class="handlediv" title="Click to toggle"><br></div>
-					<h3 class="hndle ui-sortable-handle"><span>Select:</span> </h3>
-
-					<div class="inside">
-						<div class="tribe-tickets-input tribe-tickets-input-text">
-							<label for="tickets_attendee_info_field">Label:</label>
-							<input type="text" name="tickets_attendee_info_field" value="">
-						</div>
-						<div class="tribe-tickets-input tribe-tickets-input-textarea">
-							<label for="tickets_attendee_info_field">Options (one per line)</label>
-							<textarea name="tickets_attendee_info_field" value="" rows="5"></textarea>
-						</div>
-						<div class="tribe-tickets-input tribe-tickets-input-checkbox tribe-tickets-required">
-							<label class="prompt"><input type="checkbox" name="tickets_attendee_info_field_required">
-								Required?</label>
-						</div>
-						<div class="tribe-tickets-delete-field">
-							<a href="#">Delete this field</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</td>
-	</tr>
-	</tbody>
-</table>
+				</td>
+			</tr>
+		</table>
+	</td>
+</tr>
